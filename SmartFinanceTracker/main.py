bca01cbcaf8c1ff756c497669156965354273d03
Vinayak Tiwari <159702509+vintiw6@users.noreply.github.com>
2025-03-06 23:18:39 +0530

    added csv data and completed the basic version

--- SmartFinanceTracker/main.py
+++ SmartFinanceTracker/main.py
@@ -1,7 +1,10 @@
 import pandas as pd
 import csv
+import matplotlib.pyplot as plt
 from datetime import datetime
 from data_entry import get_amount, get_category, get_date, get_description
+
+
 
 class CSV:
     CSV_FILE = "finance_data.csv"
@@ -72,23 +75,55 @@ def add():
     description = get_description()
     CSV.add_entry(date, amount, category, description) 
 
-# Example call
-CSV.get_transactions("01-01-2023", "30-07-2024")  
 
+def plot_transactions(df):
+    df.set_index('date',inplace = True)
+
+    income_df = (
+        df[df["category"] == "Income"]
+        .resample("D")
+        .sum()
+        .reindex(df.index, fill_value=0)
+        )
+    expense_df = (
+        df[df["category"] == "Expense"]
+        .resample("D")
+        .sum()
+        .reindex(df.index, fill_value=0)
+        )
+    
+    plt.figure(figsize=(10,5))
+    plt.plot(income_df.index, income_df["amount"], label="Income", color="g")
+    plt.plot(expense_df.index, expense_df["amount"], label="Expense", color="r")
+    plt.xlabel("Date")
+    plt.ylabel("Amount")
+    plt.title('Income and Expenses Over Time')
+    plt.legend()
+    plt.grid(True)
+    plt.show()
 
 
 def main ():
     while True:
-        print("\n1. Add a new Transaction")
+        print("\n1.Add a new Transaction")
         print("2.View transcations and summary within a date range")
         print("3.Exit")
+
+        choice = input("Enter your choice: ")
 
         if choice == "1":
            add()
         elif choice =="2":
             start_date = get_date("Enter the start date(dd-mm-yyyy): ")
             end_date = get_date("Enter the end date(dd-mm-yyyy): ")
-            CSV.get_transactions(start_date , end_date)
+            df = CSV.get_transactions(start_date , end_date)  # Ensure CSV.get_transactions exists
+            if input("Do you want to see a plot? (y/n)").lower() == "y":
+                plot_transactions(df)
         elif choice =="3":
-            print("Exiting ")
+            print("Exiting...")
             break
+        else:
+            print("Invalid choice. Enter 1, 2 or 3.")
+
+if __name__ == "__main__":
+    main()
