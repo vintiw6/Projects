10a151afafad928bcaa3df814ba819877ce9be54
Vinayak Tiwari <159702509+vintiw6@users.noreply.github.com>
2025-03-16 19:40:16 +0530

    added few new things

---
+++ SmartFinanceTracker/main.py
@@ -0,0 +1,47 @@
+import pandas as pd
+import csv
+from datetime import datetime
+from data_entry import get_amount , get_category , get_date , get_description
+
+class CSV:
+    CSV_FILE = "finance_data.csv"
+    COLUMNS = ["date","amount","category","description"]
+
+    @classmethod
+    def initialize_csv(cls):
+        try:
+            pd.read_csv(cls.CSV_FILE)
+        except FileNotFoundError:
+            df = pd.DataFrame(columns=cls.COLUMNS)
+            df.to_csv(cls.CSV_FILE, index=False)
+
+
+
+    @classmethod
+    def add_entry(cls,date,amount,category,description):
+        new_entry = {
+            "date": date,
+            "amount": amount,
+            "category": category,
+            "description": description
+        }
+        with open(cls.CSV_FILE, "a", newline="") as csvfile:
+            writer = csv.DictWriter(csvfile, fieldnames = cls.COLUMNS)
+            writer.writerow(new_entry)
+        print("Entry added sucessfully")
+
+def add():
+    CSV.initialize_csv()
+    date = get_date("Enter the date of the transaction (dd-mm-yyyy) or enter for today's date: ", allowed_default=True)
+    amount = get_amount()
+    category = get_category()
+    description = get_description()
+    CSV.add_entry(date, amount, category, description)
+
+add()
+        
+        
+# CSV.initialize_csv()
+# CSV.add_entry("20-07-2024",125.65,"Income","Salary")
+
+
