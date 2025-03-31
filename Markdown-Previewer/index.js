4b0f0861b244eb11ad06903c19724a3b4de5b5a4
Vinayak Tiwari <159702509+vintiw6@users.noreply.github.com>
2025-03-31 20:02:11 +0530

    added a new project

---
+++ Markdown-Previewer/index.js
@@ -0,0 +1,6 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import MarkdownPreviewer from "./App";
+import "./index.css";
+
+ReactDOM.render(<MarkdownPreviewer />, document.getElementById("root"));
