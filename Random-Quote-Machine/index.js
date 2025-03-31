69bc3f074c76db936f35c942ab5fe81ac9055f38
Vinayak Tiwari <159702509+vintiw6@users.noreply.github.com>
2025-03-31 19:56:39 +0530

    added a new project

---
+++ Random-Quote-Machine/index.js
@@ -0,0 +1,6 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import QuoteMachine from "./App";
+import "./index.css";
+
+ReactDOM.render(<QuoteMachine />, document.getElementById("root"));
