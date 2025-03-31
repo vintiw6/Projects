523a7f4f2b2381ffcb6472a63162f60943640dd4
Vinayak Tiwari <159702509+vintiw6@users.noreply.github.com>
2025-03-31 21:06:39 +0530

    Delete Random-Quote-Machine directory

--- Random-Quote-Machine/index.js
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import QuoteMachine from "./App";
-import "./index.css";
-
-ReactDOM.render(<QuoteMachine />, document.getElementById("root"));
